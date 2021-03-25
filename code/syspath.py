d7315484121d42a9ab2c53771309ef432be0c79b
Hunmin Yang <beachmind2@gmail.com>
2021-03-25 23:38:25 +0900

    com

---
+++ code/syspath.py
@@ -0,0 +1,9 @@
+import sys
+
+print(sys.path, "\n")
+
+sys.path.append("/Users/beachmind/github/python-basics/code/mymod")
+print(sys.path, "\n")
+
+#sys.path.remove("/Users/beachmind/github/python-basics/code/mymod")
+#print(sys.path)
